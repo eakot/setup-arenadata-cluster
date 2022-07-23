5e4b3fa6540e1777920becda895afec9f0b84e41
Egor Kotelnikov <eakotelnikov@ya.ru>
2022-07-23 21:45:34 +0100

    Update install_adcm.sh

--- scripts/install_adcm.sh
+++ scripts/install_adcm.sh
@@ -19,8 +19,7 @@ sudo docker create --name adcm -p 8000:8000 -v /opt/adcm:/adcm/data docker.io/ar
 sudo docker start adcm
 
 sudo -- su -c "yum install -y wget"
-sudo yum -y install https://dl.fedoraprojДев
-ect.org/pub/epel/epel-release-latest-7.noarch.rpm
+sudo yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 sudo -- su -c "yum install -y jq"
 
 timeout 30 bash -c 'while [[ "$(curl --insecure -s -o /dev/null -w ''%{http_code}'' -d "username=admin&password=admin" -X POST localhost:8000/api/v1/token/)" != "200" ]]; do echo "wait docker run (up to 5 min)..."; sleep 20; done'
